c86b0337bde4f22eeeeb84e34d690076040dedf4
kurs <kurs@kurs-Komputer>
2018-03-09 14:25:08 +0100

    Klasa Waluta - podstawowe definicje

--- src/waluty/Waluta.java
+++ src/waluty/Waluta.java
@@ -1,7 +1,73 @@
 package waluty;
 
+import java.math.BigDecimal;
+
 public class Waluta {
 
+	private final String kod;
+	private final String nazwa;
+	private final BigDecimal kurs;
+	
+	public Waluta(String kod, String nazwa, BigDecimal kurs) {
+		this.kod = kod;
+		this.nazwa = nazwa;
+		this.kurs = kurs;
+	}
+
+	public String getKod() {
+		return kod;
+	}
+
+	public String getNazwa() {
+		return nazwa;
+	}
+
+	public BigDecimal getKurs() {
+		return kurs;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s (%s): %s", kod, nazwa, kurs);
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kod == null) ? 0 : kod.hashCode());
+		result = prime * result + ((kurs == null) ? 0 : kurs.hashCode());
+		result = prime * result + ((nazwa == null) ? 0 : nazwa.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Waluta other = (Waluta) obj;
+		if (kod == null) {
+			if (other.kod != null)
+				return false;
+		} else if (!kod.equals(other.kod))
+			return false;
+		if (kurs == null) {
+			if (other.kurs != null)
+				return false;
+		} else if (!kurs.equals(other.kurs))
+			return false;
+		if (nazwa == null) {
+			if (other.nazwa != null)
+				return false;
+		} else if (!nazwa.equals(other.nazwa))
+			return false;
+		return true;
+	}
 	
 	
-}
+	
+}	
